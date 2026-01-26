3ed9a139999a72a3af47ebcfce14107d084a209f
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-01-26 13:03:10 -0500

    Finish home page

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import homeImagePath from "./food.jpg"
+
 function load(){
     let contentDiv = document.querySelector("div#content");
 
@@ -7,10 +9,38 @@ function load(){
     }
 
     // load home page
-    let bigHeader = document.createElement("h1");
+    const bigHeader = document.createElement("h1");
     bigHeader.textContent = "Welcome to the Odin Restaurant!"
 
-    contentDiv.appendChild(bigHeader);
+    const homeIntro = document.createElement("h2");
+    homeIntro.textContent = "It's like Olive Garden, but healthier for your stomach and brain!"
+
+    const homeImage = document.createElement("img");
+    homeImage.src = homeImagePath;
+    homeImage.width = 640;
+    homeImage.alt = "Picture of food variety";
+
+    const whyDineHere = document.createElement("h2");
+    whyDineHere.textContent = "Why dine here?";
+
+    const reasonsToDineHere = document.createElement("ul");
+    const reasons = [
+        "Tasty and nutritional food~",
+        "Download our free app (No ads, only food) for discounts, home delivery and diet tracking!",
+        "Recipes are budget friendly, free and open sourced. Share with your friends!",
+        "Experience historical dishes from different cultures~",
+        "Awesome unionized staff~"
+    ]   
+    for (const reason of reasons){
+        const reasonListItem = document.createElement("li");
+        reasonListItem.textContent = reason;
+        reasonsToDineHere.appendChild(reasonListItem);
+    }
+
+    const allElements = [bigHeader, homeIntro, homeImage, whyDineHere, reasonsToDineHere];
+    for (const element of allElements){
+        contentDiv.appendChild(element);
+    }
 }
 
 export {load}
