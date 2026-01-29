0bc32440ff9f42034d9014d51be1dd8ed297b262
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-01-29 14:44:53 -0500

    Add tab switching functionality
    Nav bar buttons can switch between pages using clearContentDiv
    and load functions in each page's module.

--- src/index.js
+++ src/index.js
@@ -7,4 +7,26 @@ import "./styles.css"
 
 console.log("index.js: Hello World")
 
-about.load();
+// Load into home on visit
+home.load();
+
+// Query for the nav buttons
+const navHomeBtn = document.querySelector("button#navHome");
+const navMenuBtn = document.querySelector("button#navMenu");
+const navAboutBtn = document.querySelector("button#navAbout");
+
+// Add event listeners to the nav bar's tab buttons.
+navHomeBtn.addEventListener("click", () => {
+    clearContentDiv.clear();
+    home.load();
+});
+
+navMenuBtn.addEventListener("click", () => {
+    clearContentDiv.clear();
+    menu.load();
+});
+
+navAboutBtn.addEventListener("click", () => {
+    clearContentDiv.clear();
+    about.load();
+});
