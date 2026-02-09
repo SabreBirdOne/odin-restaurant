649f97024797fd5d52b90628372b13e004364ee6
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-02-09 17:04:14 -0500

    Change config files to use webpack-merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,21 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
+  entry: {
+    app: './src/index.js',
   },
-
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Production',
       template: "./src/template.html",
     }),
   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(import.meta.dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
